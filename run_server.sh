73412b46f18146067012446a78ce42811cfdc9fd
Francis Tseng <f+accounts@frnsys.com>
2018-02-15 16:42:54 -0500

    scripts to make server and changes easier

---
+++ run_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Which site do you want to run? (e.g. basic_1) " SITE
+
+if [ ! -d "$SITE" ]; then
+    echo "That site doesn't seem to exist"
+    exit 1
+fi
+
+echo "Running the server now. Press Ctrl+C a few times to shut it down"
+echo "Open up localhost:8000 in your browser"
+cd $DIR/$SITE
+python2.7 -m SimpleHTTPServer 8000
---
+++ upload_changes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Briefly describe your changes: " CHANGES
+git add -A
+git commit -m "${CHANGES}"
+
+echo "Pushing your changes now."
+echo "You'll be prompted for your GitHub username and password."
+git push
