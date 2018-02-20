eed2f765ea95b31138c78b31da36c995db56f60c
newschool <newschool@b261-01.tns.newschool.edu>
2018-02-20 12:07:38 -0500

    tweaks

--- run_server.sh
+++ run_server.sh
@@ -3,7 +3,7 @@
 DIR=$(dirname "$0")
 cd "$DIR"
 
-echo -e "\n\n"
+echo -e "\n\n------------------------------------------"
 read -p "Which site do you want to run? (e.g. basic_1) " SITE
 
 if [ ! -d "$SITE" ]; then
@@ -11,7 +11,10 @@ if [ ! -d "$SITE" ]; then
     exit 1
 fi
 
-echo -e "\n\nRunning the server now. Press Ctrl+C a few times to shut it down"
+echo -e "\n\n------------------------------------------"
+echo -e "Running the server now. Press Ctrl+C a few times to shut it down"
 echo -e "Open up localhost:8000 in your browser\n\n"
+echo -e "------------------------------------------\n\n"
+
 cd "$DIR/$SITE"
 python2.7 -m SimpleHTTPServer 8000
--- upload_changes.sh
+++ upload_changes.sh
@@ -3,11 +3,13 @@
 DIR=$(dirname "$0")
 cd "$DIR"
 
-echo -e "\n\n"
+echo -e "\n\n------------------------------------------"
 read -p "Briefly describe your changes: " CHANGES
 git add -A
 git commit -m "${CHANGES}"
 
-echo -e "\n\nPushing your changes now."
-echo -e "You'll be prompted for your GitHub username and password.\n\n"
+echo -e "\n\n------------------------------------------"
+echo -e "Pushing your changes now."
+echo -e "You'll be prompted for your GitHub username and password."
+echo -e "------------------------------------------\n\n"
 git push
